677f168101015b114ccd9401f6ab5fdae698b68f
spacecaker <robin-barry@hotmail.com>
2013-04-19 13:28:35 +0300

    revert

--- src/com/android/settings/cyanogenmod/SystemSettings.java
+++ src/com/android/settings/cyanogenmod/SystemSettings.java
@@ -40,11 +40,9 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     private static final String KEY_FONT_SIZE = "font_size";
     private static final String KEY_NOTIFICATION_DRAWER = "notification_drawer";
     private static final String KEY_NOTIFICATION_DRAWER_TABLET = "notification_drawer_tablet";
-    private static final String PREF_RECENT_APP_SWITCHER = "recent_app_switcher";    
     private static final String KEY_NAVIGATION_BAR = "navigation_bar";
 
     private ListPreference mFontSizePref;
-    private ListPreference mRecentAppSwitcher;
 
     private final Configuration mCurConfig = new Configuration();
     
@@ -53,12 +51,6 @@ public class SystemSettings extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.system_settings);
-        
-        mRecentAppSwitcher = (ListPreference) findPreference(PREF_RECENT_APP_SWITCHER);
-        mRecentAppSwitcher.setOnPreferenceChangeListener(this);
-        mRecentAppSwitcher.setValue(Integer.toString(Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.RECENT_APP_SWITCHER,
-                0)));        
 
         mFontSizePref = (ListPreference) findPreference(KEY_FONT_SIZE);
         mFontSizePref.setOnPreferenceChangeListener(this);
@@ -138,14 +130,11 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mRecentAppSwitcher) {
-                           
-            Settings.System.putInt(getActivity().getContentResolver());
-            Settings.System.RECENT_APP_SWITCHER;
-            Helpers.restartSystemUI();
-
-            return true;
+        final String key = preference.getKey();
+        if (KEY_FONT_SIZE.equals(key)) {
+            writeFontSizePreference(objValue);
         }
-        return false;
+
+        return true;
     }
 }

77e2562cf1d100fabbfeddd00c55299456fe4e86
spacecaker <robin-barry@hotmail.com>
2013-05-06 12:37:14 +0300

    not yet fixed giving class, interface, enum expected error

--- src/com/android/settings/SystemUITweaks.java
+++ src/com/android/settings/SystemUITweaks.java
@@ -13,46 +13,47 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
 import android.util.Log;
 import android.text.Spannable;
 import android.widget.EditText;
 
 import com.android.settings.R;
-import com.android.settings.util.SeekBarPreference;
-import com.android.settings.util.Helpers;
-import com.android.settings.util.colorpicker.ColorPickerPreference;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class SystemUITweaks extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 	
-	private boolean isTablet;
+        private boolean isTablet;
 
-    private static final String PREF_RECENT_APP_SWITCHER = "recent_app_switcher";
+   private static final String PREF_RECENT_APP_SWITCHER ="recent_app_switcher";
 
-    private ListPreference mRecentAppSwitcher;
+   private ListPreference mRecentAppSwitcher;
    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.recents_tweaks);
-        PreferenceScreen prefSet = getPreferenceScreen();
-        
-        mRecentAppSwitcher = (ListPreference) findPreference(PREF_RECENT_APP_SWITCHER);
-        mRecentAppSwitcher.setOnPreferenceChangeListener(this);
-        mRecentAppSwitcher.setValue(Integer.toString(Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.RECENT_APP_SWITCHER,
-                0)));
-        }
-    }
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       addPreferencesFromResource(R.xml.recents_tweaks);
+ 
+       PreferenceScreen prefSet = getPreferenceScreen();
+         
+       mRecentAppSwitcher = (ListPreference) findPreference(PREF_RECENT_APP_SWITCHER);
+       mRecentAppSwitcher.setOnPreferenceChangeListener(this);
+       mRecentAppSwitcher.setValue(Integer.toString(Settings.System.getInt(getActivity()
+               .getContentResolver(), Settings.System.RECENT_APP_SWITCHER,
+               0)));
+       }
+   }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mRecentAppSwitcher) {
-		
-            int val = Integer.parseInt((String) newValue);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                Settings.System.RECENT_APP_SWITCHER, val);
-            Helpers.restartSystemUI();
-            return true;
-        }
-        return false;
-    }
+   @Override
+   public boolean onPreferenceChange(Preference preference, Object newValue) {
+       if (preference == mRecentAppSwitcher) {
+
+           int val = Integer.parseInt((String) newValue);
+           Settings.System.putInt(getActivity().getContentResolver(),
+               Settings.System.RECENT_APP_SWITCHER, val);
+           return true;
+       }
+       return false;
+   }
